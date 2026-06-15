0a65e31411109335a87f5e6dd6352abd6146ad40
Thomas N. Jensen <thomasnorupjensen@gmail.com>
2026-06-15 09:37:24 +0200

    Edited contact form button color + error.js

--- src/app/error.js
+++ src/app/error.js
@@ -14,9 +14,9 @@ export default function Error({ error, reset }) {
   }, [error]);
 
   return (
-    <div>
-      {/* <h2>Something went wrong</h2>
-      <button onClick={() => reset()}>Try again</button> */}
-    </div>
+    {/* <div>
+      <h2>Something went wrong</h2>
+      <button onClick={() => reset()}>Try again</button>
+    </div> */}
   );
 }
