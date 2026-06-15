afea4529a4055ff86abf0aee7eaab11235c8a9a4
Thomas N. Jensen <thomasnorupjensen@gmail.com>
2026-06-15 17:57:51 +0200

    Edited formatting

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,22 +2,22 @@ import { Post } from '@/lib/types';
 import { Suspense } from 'react';
 import Loading from './loading';
 
-export default async function Projects(){
+export default async function Projects() {
 
     const response = await fetch('https://noroff.tnjensen.com/portfolio/wp-json/wp/v2/posts?categories=6,9&_embed&filter[orderby]=date&order=asc')
     const data = await response.json()
-    
-    return(
+
+    return (
         <div className="grid grid-cols-12 mx-auto px-10 pb-6">
-           <h1 className="py-4 text-center col-span-12">Blog</h1>
-           <Suspense fallback={<Loading />}> 
-            {data?.map((item:Post) => (
-                <div className="flex flex-col bg-background rounded max-w-xs px-4 py-3 m-2 col-span-12 mx-auto text-center md:col-span-6 lg:col-span-4" key={item.id}>
-                <h2 dangerouslySetInnerHTML={{__html: item.title.rendered}} className="text-xl mb-2"></h2>
-                {item._embedded['wp:featuredmedia'] && <img src={item._embedded['wp:featuredmedia'][0].source_url} alt={item._embedded['wp:featuredmedia'][0].alt_text} className="mb-3" /> }
-                <span dangerouslySetInnerHTML={{__html: item.content.rendered}}></span>
-                </div>
-            ))}
+            <h1 className="py-4 text-center col-span-12">Blog</h1>
+            <Suspense fallback={<Loading />}>
+                {data?.map((item: Post) => (
+                    <div className="flex flex-col bg-background rounded max-w-xs px-4 py-3 m-2 col-span-12 mx-auto text-center md:col-span-6 lg:col-span-4" key={item.id}>
+                        <h2 dangerouslySetInnerHTML={{ __html: item.title.rendered }} className="text-xl mb-2"></h2>
+                        {item._embedded['wp:featuredmedia'] && <img src={item._embedded['wp:featuredmedia'][0].source_url} alt={item._embedded['wp:featuredmedia'][0].alt_text} className="mb-3" />}
+                        <span dangerouslySetInnerHTML={{ __html: item.content.rendered }}></span>
+                    </div>
+                ))}
             </Suspense>
         </div>
     )
