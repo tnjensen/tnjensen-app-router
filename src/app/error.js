f27d74f2015c32ec8e105f0a2b58880938ea01ec
Thomas N. Jensen <thomasnorupjensen@gmail.com>
2026-06-15 09:30:12 +0200

    Edited error.js

--- src/app/error.js
+++ src/app/error.js
@@ -15,8 +15,8 @@ export default function Error({ error, reset }) {
 
   return (
     <div>
-      <h2>Something went wrong</h2>
-      <button onClick={() => reset()}>Try again</button>
+      {/* <h2>Something went wrong</h2>
+      <button onClick={() => reset()}>Try again</button> */}
     </div>
   );
 }
