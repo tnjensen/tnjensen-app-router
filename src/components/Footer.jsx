c89ee2f5496aa61dd5655142adb45b85988f3b60
Thomas N. Jensen <thomasnorupjensen@gmail.com>
2026-06-19 14:44:19 +0200

    Edited footer layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,14 +11,14 @@ export function Footer(){
     return(
         <footer className="bg-foreground text-white h-12 p-2 bottom-0 z-20">
             <div className="flex h-8 items-center justify-around w-full">
-                <p>Copyright &copy; {date}</p> - Thomas N. Jensen
+                <p>Copyright &copy; {date} - Thomas N. Jensen</p>
                 <div className="flex items-center gap-2">
                     <Link href={"https://www.linkedin.com/in/tnjensen09/"}><FaLinkedin /></Link>
                     <Link href={"https://www.instagram.com/tnjensen09/"}><FaInstagram /></Link>
                     <Link href={"https://www.facebook.com/profile.php?id=100058563252363"}><FaFacebook /></Link>
                 </div>
-                <div className='text-center mt-2'>Sidene er utviklet med NextJS</div>
             </div>
+            <div className='text-center'>Sidene er utviklet med NextJS</div>
     </footer>
     )
 }
